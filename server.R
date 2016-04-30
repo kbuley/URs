0418249e61d1b0f96b6b152dfdfee44cef4e29d9
Kevin Buley <kevin@buley.org>
2016-04-30 12:43:50 -0400

    It works!

--- server.R
+++ server.R
@@ -1,17 +1,26 @@
 library(shiny)
+library(plyr)
+#library(rgeos)
+#library(maptools)
+library(ggplot2)
+FortifiedSAT <- readRDS(file = "FortifiedSAT.rds")
+SATURs <- read.csv(file="SAT UR Data.csv", header = TRUE)
+SATURs$Date <- as.Date(SATURs$Date, "%m/%d/%Y")
+SATURs$State <- as.character(SATURs$State)
 
 # Main server function
 shinyServer(function(input, output) {
 
-  output$distPlot <- renderPlot({
+   output$SATStates <- renderPlot({
+    dateSubSet <- SATURs[SATURs$Date %in% input$dates1:input$dates2,]
+    switch(input$select,
+           "UNC"={meanURs <- ddply(dateSubSet, .(State), summarize, meanType = mean(Uncommented))},
+           "OP"={meanURs <- ddply(dateSubSet, .(State), summarize, meanType = mean(Open))},
+           "RLC"={meanURs <- ddply(dateSubSet, .(State), summarize, meanType = mean(RLC))}
+    )
 
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+    ggplot() + geom_map(data = meanURs,
+                        aes(map_id = State, fill = meanType),
+                        map = FortifiedSAT) + expand_limits(x = FortifiedSAT$long, y = FortifiedSAT$lat)
   })
-
 })
--- ui.R
+++ ui.R
@@ -1,24 +1,26 @@
 library(shiny)
 
 # Define UI for application
-shinyUI(fluidPage(
+shinyUI(fluidPage(title = "Request Stats",
 
   # Application title
-  titlePanel("Waze Update Request stats"),
+  titlePanel(h1("Waze Update Request Stats", align="center")),
 
   # Sidebar
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+      #set up the panel with start and end dates to look at
+       dateInput("dates1", label = h3("Start Date"), value = "2001-01-01"),
+       dateInput("dates2", label = h3("End Date")),
+       #which type of UR do we want to look at
+       selectInput("select", label = h3("Select UR State"),
+                   choices = list("Uncommented" = "UNC", "Open" = "OP",
+                                  "RLC" = "RLC"), selected = "UNC")
     ),
 
     # The main application Window
     mainPanel(
-       plotOutput("distPlot")
+       plotOutput("SATStates")
     )
   )
 ))
